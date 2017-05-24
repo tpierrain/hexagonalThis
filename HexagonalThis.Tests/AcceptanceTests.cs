e0e08aca14e102ca4dab677c02d35c52b31d730b
Thomas PIERRAIN <thomas@pierrain.net>
2017-05-24 23:44:22 +0200

    Add second failing test: Should_give_some_verses_when_asking_lines_to_the_hexagon() which introduce the concept of hexagon and mock for right adapter (with interface: IKnowLotsOfPoetry)

--- HexagonalThis.Tests/AcceptanceTests.cs
+++ HexagonalThis.Tests/AcceptanceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NFluent;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace HexagonalThis.Tests
@@ -7,16 +8,50 @@ namespace HexagonalThis.Tests
     public class AcceptanceTests
     {
         [Test]
-        public void Should_give_some_poetry_fragment_when_asking_lines()
+        public void Should_give_some_verses_when_asking_lines()
         {
             var poet = new Poet();
             var verses = poet.GiveMeVerses(1);
 
             Check.That(verses).IsEqualTo("Souvent, pour s\'amuser, les hommes d\'équipage");
         }
+
+        [Test]
+        public void Should_give_some_verses_when_asking_lines_to_the_hexagon()
+        {
+            var poetryProvider = Substitute.For<IKnowLotsOfPoetry>();
+            poetryProvider.FindPoem().Returns("If you could read a leaf or tree\r\nyou’d have no need of books.\r\n-- © Alistair Cockburn (1987)");
+
+            var hexagon = new Hexagon(poetryProvider);
+
+            Check.That(hexagon.GiveMeVerses(2)).IsEqualTo("If you could read a leaf or tree\r\nyou’d have no need of books.");
+        }
     }
 
-    public class Poet
+    public class Hexagon : IProvideVerses
+    {
+        public string GiveMeVerses(int numberOfVerse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hexagon(IKnowLotsOfPoetry poetryProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IKnowLotsOfPoetry
+    {
+        string FindPoem();
+    }
+
+    public interface IProvideVerses
+    {
+        string GiveMeVerses(int numberOfVerse);
+    }
+
+    public class Poet : IProvideVerses
     {
         public string GiveMeVerses(int numberOfVerse)
         {
