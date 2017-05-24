ab42acfdddbde15fa090b70c959125a016c097e6
Thomas PIERRAIN <thomas@pierrain.net>
2017-05-24 23:16:12 +0200

    Add a first acceptance test

--- HexagonalThis.Tests/AcceptanceTests.cs
+++ HexagonalThis.Tests/AcceptanceTests.cs
@@ -1,10 +1,26 @@
-﻿using NFluent;
+﻿using System;
+using NFluent;
 using NUnit.Framework;
 
 namespace HexagonalThis.Tests
 {
     public class AcceptanceTests
     {
-        
+        [Test]
+        public void Should_give_some_poetry_fragment_when_asking_lines()
+        {
+            var poet = new Poet();
+            var verses = poet.GiveMeVerses(1);
+
+            Check.That(verses).IsEqualTo("Souvent, pour s\'amuser, les hommes d\'équipage");
+        }
+    }
+
+    public class Poet
+    {
+        public string GiveMeVerses(int numberOfVerse)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
