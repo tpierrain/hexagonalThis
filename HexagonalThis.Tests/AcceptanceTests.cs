bc5da0f7ea804f7e22e4e17f5becf082f022da23
Thomas PIERRAIN <thomas@pierrain.net>
2017-06-11 19:36:09 +0200

    First step

--- HexagonalThis.Tests/AcceptanceTests.cs
+++ HexagonalThis.Tests/AcceptanceTests.cs
@@ -1,10 +1,29 @@
 ﻿using NFluent;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace HexagonalThis.Tests
 {
     public class AcceptanceTests
     {
+        [Test]
+        public void Should_give_verses_when_asking_poetry()
+        {
+            // Simplest Driver possible
+            // port: Poet.GiveMeSomePoetry();
+            var poet = new Poet();
+            var verses = poet.GiveMeSomePoetry();
+
+            Check.That(verses).IsEqualTo("If you could read a leaf or tree\r\nyou’d have no need of books.\r\n-- © Alistair Cockburn (1987)");
+        }
         
+    }
+
+    public class Poet
+    {
+        public object GiveMeSomePoetry()
+        {
+            return "If you could read a leaf or tree\r\nyou’d have no need of books.\r\n-- © Alistair Cockburn (1987)";
+        }
     }
 }
